e2755b958ea9085ffe88cd10b31d6d5a66968e2d
Zarzand <Zarzand>
2019-04-24 20:23:03 +0300

    login and registration templates added

---
+++ src/main/java/com/petros/mailapplication/security/AuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.petros.mailapplication.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petros.mailapplication.ui.model.request.UserLoginRequestModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import io.jsonwebtoken;
+//import io.jsonwebtoken.Jwts;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            UserLoginRequestModel creds=new ObjectMapper().readValue(request.getInputStream(),UserLoginRequestModel.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String userName=((User) authResult.getPrincipal()).getUsername();
+        String token= Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis()+SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512,SecurityConstants.TOKEN_SECRET)
+                .compact();
+        response.addHeader(SecurityConstants.HEADER_STRING,SecurityConstants.TOKEN_PREFIX+token);
+    }
+}
